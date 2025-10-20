4ccc0d40091404eb1943b1eb1c3d1a023b8efb63
Meltinggg <lizhe15889947323@gmail.com>
2025-10-20 09:39:51 +0800

    Add files via upload
    This program aims to evaluate whether the median or the average is more accurate in income surveys under different circumstances.

---
+++ income_research.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int N = 0;
+	int middle = 0;
+	int average = 0;
+	int total = 0;
+	
+	cout<<"enter the number of survey respondents："; cin>>N;
+
+	int* income = new int[N];
+
+	for(int i=0;i < N+1;i++){
+		printf("enter No.%d 's income：", i+1);
+		cin>>income[i]; cout<<endl;
+		total = total + income[i];
+	}
+
+	average = total / N;
+	
+	int m = N % 2;
+	int k = (N+m) / 2;
+
+	for(int i=1;i < N-1;i++){	
+		int min=0;
+		for(int j=i;j < N;j++){
+			if(income[j] < income[min]){
+				min = j;
+			}
+		}
+		int temp = income[i];
+		income[i] = income[min];
+		income[min] = temp;
+	}
+
+	middle = income[k];
+
+	cout<<"median："<<middle<<endl;
+	cout<<"average："<<average<<endl;
+
+
+	return 0;
+}
